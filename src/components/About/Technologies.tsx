1097dc5c67ff9d504fbd4e71e813d797aa494eff
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-08 08:13:55 +0200

    add contact form

--- src/components/About/Technologies.tsx
+++ src/components/About/Technologies.tsx
@@ -1,57 +1,37 @@
+import { motion } from "framer-motion";
+
 const Technologies = () => {
   return (
     <div className="w-full text-center flex flex-col justify-center items-center gap-10 py-6 mb-16">
-      <h2 className="tracking-wide">Technologies</h2>
+      <motion.h2
+        className="tracking-wide"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1 }}
+      >
+        Technologies
+      </motion.h2>
       <div className="w-full flex flex-wrap justify-evenly items-center">
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg"
-            alt="Django"
-          />
-        </span>
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
-            alt="React"
-          />
-        </span>
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg"
-            alt="Node.js"
-          />
-        </span>
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
-            alt="MySQL"
-          />
-        </span>
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-            alt="TailwindCss"
-          />
-        </span>
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg"
-            alt="TypeScript"
-          />
-        </span>
-        <span>
-          <img
-            className="w-24 h-24"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/archlinux/archlinux-original.svg"
-            alt="TypeScript"
-          />
-        </span>
+        {[
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg",
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg",
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg",
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg",
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/archlinux/archlinux-original.svg",
+        ].map((src, index) => (
+          <motion.span
+            key={index}
+            initial={{ x: index % 2 === 0 ? -200 : 200, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: index * 0.2 }}
+          >
+            <img className="w-24 h-24" src={src} alt="Technology" />
+          </motion.span>
+        ))}
       </div>
     </div>
   );
--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -3,12 +3,12 @@ import { useForm, ValidationError } from "@formspree/react";
 const ContactForm = () => {
   const [state, handleSubmit] = useForm("xvoejykk");
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return <p className="text-4xl text-ivory">Thanks for joining!</p>;
   }
   return (
     <form
       onSubmit={handleSubmit}
-      className="p-6 rounded-lg shadow-lg flex flex-col justify-center items-center gap-4"
+      className="p-6 rounded-lg flex flex-col justify-center items-center gap-4 w-full sm:w-[700px]"
     >
       <h3 className="text-ivory text-3xl w-full text-center mb-10">
         Contact <span>Me</span>
@@ -21,12 +21,24 @@ const ContactForm = () => {
         type="email"
         name="email"
         placeholder="john.doe@email.com"
-        className="w-full rounded-lg text-center sm:text-start h-8 px-2 bg-[#014040] focus:border-none"
+        className="w-full rounded-lg text-center text-ivory h-8 px-2 bg-[#014040]"
       />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <textarea id="message" name="message" />
+      <label htmlFor="message" className="text-ivory">
+        Message
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Your message here..."
+        className="w-full h-[300px] sm:h-[500px] bg-[#014040] text-ivory rounded-lg text-start p-4"
+      />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button type="submit" disabled={state.submitting}>
+      <button
+        type="submit"
+        disabled={state.submitting}
+        className="border-2 border-ivory py-2 px-3 text-pine bg-ivory font-bold transition-all duration-300 hover:text-ivory hover:bg-transparent"
+      >
         Submit
       </button>
     </form>
