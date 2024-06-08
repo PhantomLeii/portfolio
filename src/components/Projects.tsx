dbf28e3cc6c1d82bcb9efa5e125434a7d8d09397
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-08 11:57:58 +0200

    Update: add projects animation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,20 +1,29 @@
+import { motion } from "framer-motion";
 import { Project } from "@/data/interfaces";
 
 const Projetcs = ({ projectData }: { projectData: Project[] }) => {
   return (
     <section
       id="projects"
-      className="w-full py-6 flex justify-center items-center"
+      className="w-full py-6 flex justify-center items-center overflow-x-hidden"
     >
       <div className="wrapper flex flex-col justify-center items-center gap-16 w-full">
-        <h1 className="w-full text-center">
+        <motion.h1
+          className="w-full text-center"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.58 }}
+        >
           My <span className="text-teal">Projects</span>
-        </h1>
+        </motion.h1>
         <div className="flex flex-wrap justify-center items-center gap-4">
           {projectData.map((item, i) => (
-            <div
+            <motion.div
               key={i}
               className="projectCard bg-pine shadow-md shadow-[rgba(0,0,0,0.6)] p-4 w-full max-w-[370px] rounded-xl transition-all duration-300 hover:-translate-y-2 z-10"
+              initial={{ y: 50, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.58, delay: i * 0.5 }}
             >
               <div className="h-1/2 mb-2">
                 <img
@@ -45,7 +54,7 @@ const Projetcs = ({ projectData }: { projectData: Project[] }) => {
                   </span>
                 ))}
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
