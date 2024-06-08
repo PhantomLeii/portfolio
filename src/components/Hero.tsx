24cdc1c10e4ec139e7e63a8dd9286c2d36586923
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-08 10:34:45 +0200

    add Hero animations

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { RiArrowRightDownLine } from "react-icons/ri";
 
 const Hero = () => {
@@ -7,24 +8,45 @@ const Hero = () => {
       className="py-6 flex flex-col justify-center items-center h-[calc(100vh-80px)]"
     >
       <div className="wrapper w-full text-center flex flex-col justify-center items-center ">
-        <p className="uppercase text-sm tracking-widest">
+        <motion.p
+          className="uppercase text-sm tracking-widest"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.58 }}
+        >
           Dynamic web magic with reactjs
-        </p>
-        <h1 className="max-w-4xl text-3xl sm:text-4xl md:text-6xl">
+        </motion.p>
+        <motion.h1
+          className="max-w-4xl text-3xl sm:text-4xl md:text-6xl"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, delay: 0.5 }}
+        >
           Crafting seamless user experiences
           <br />
           <span className="text-teal">one line of code at a time</span>
-        </h1>
-        <p className="tracking-wide my-4">
+        </motion.h1>
+        <motion.p
+          className="tracking-wide my-4"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.58, delay: 1 }}
+        >
           Hi I&apos;m Lebogang. A passionate full stack developer based in South
           Africa
-        </p>
-        <a href="#projects" className="btn">
+        </motion.p>
+        <motion.a
+          href="#projects"
+          className="btn"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.58, delay: 1.5 }}
+        >
           <div>
             <span>See My Work</span>
             <RiArrowRightDownLine style={{ fontSize: "22px" }} />
           </div>
-        </a>
+        </motion.a>
       </div>
     </section>
   );
