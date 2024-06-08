f4ad585feb20443a81771b514c7eab3151783c90
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-08 11:51:25 +0200

    Update: add education animation

--- src/components/About/Education.tsx
+++ src/components/About/Education.tsx
@@ -1,19 +1,39 @@
+import { motion } from "framer-motion";
 import { Education } from "@/data/interfaces";
 
 const MyEducation = ({ educationData }: { educationData: Education[] }) => {
   return (
     <div
       id="education"
-      className="w-full py-6 flex flex-col justify-center items-center gap-16"
+      className="w-full py-6 flex flex-col justify-center items-center gap-16 overflow-x-hidden"
     >
-      <h2 className="text-center">Education</h2>
+      <motion.h2
+        className="text-center"
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.58 }}
+      >
+        Education
+      </motion.h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 sm:gap-12">
         {educationData.map((item, i) => (
           <>
             <div key={i} className="w-full pb-2 mb-2 border-b border-teal">
-              <h1 className="text-center sm:text-start w-full">{item.year}</h1>
+              <motion.h1
+                className="text-center sm:text-start w-full"
+                initial={{ x: -100, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.58, delay: 0.2 }}
+              >
+                {item.year}
+              </motion.h1>
             </div>
-            <div className="w-full flex flex-col justify-start items-start gap-2 sm:gap-4 mb-16 sm:mb-0">
+            <motion.div
+              className="w-full flex flex-col justify-start items-start gap-2 sm:gap-4 mb-16 sm:mb-0"
+              initial={{ x: 100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.58, delay: 0.4 }}
+            >
               <div className="pb-2 border-b-2 border-teal">
                 <h1 className="text-center sm:text-start">{item.study}</h1>
                 <h3 className="text-sm text uppercase mt-2 text-center sm:text-start">
@@ -23,7 +43,7 @@ const MyEducation = ({ educationData }: { educationData: Education[] }) => {
               <p className="text-center sm:text-start text-balance">
                 {item.desc}
               </p>
-            </div>
+            </motion.div>
           </>
         ))}
       </div>
