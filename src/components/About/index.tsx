2e57b24709f7c6b27cdcb727cb51320803f0523f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-06-08 10:52:16 +0200

    Update: add about animations

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { education } from "@/data";
 
 import Technologies from "./Technologies";
@@ -12,19 +13,38 @@ const About = ({ aboutDesc }: { aboutDesc: string }) => {
       className="w-full py-6 flex justify-center items-center mb-16"
     >
       <div className="wrapper w-full flex flex-col justify-center items-center gap-16">
-        <h1 className="w-full text-center">
+        <motion.h1
+          className="w-full text-center"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.58 }}
+        >
           About <span className="text-teal">Me</span>
-        </h1>
+        </motion.h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-6 mb-16">
-          <div className="w-full bg-noir">
+          <motion.div
+            className="w-full bg-noir rounded-lg"
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.7, delay: 0.5 }}
+          >
             <img src="/b5.svg" alt="coding" className="w-full h-full" />
-          </div>
+          </motion.div>
           <div className="w-full text-center sm:text-start flex flex-col items-start justify-start gap-4 sm:gap-8">
-            <p>{aboutDesc}</p>
-            <a
+            <motion.p
+              initial={{ x: 100, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 1 }}
+            >
+              {aboutDesc}
+            </motion.p>
+            <motion.a
               href="#education"
-              className="border-2 border-teal py-2 px-5 w-full sm:w-fit font-bold bg-teal transition-all duration-300 hover:bg-transparent hover:border-noir "
+              className="border-2 border-teal py-2 px-5 w-full sm:w-fit font-bold bg-teal transition-all duration-300 hover:bg-transparent hover:border-noir"
+              initial={{ y: 20, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.58, delay: 0.8 }}
             >
               <div className="w-full h-full flex items-center justify-center gap-2">
                 <span className="text-ivory transition-all duration-300 hover:text-noir">
@@ -32,7 +52,7 @@ const About = ({ aboutDesc }: { aboutDesc: string }) => {
                 </span>
                 <RiArrowRightDownLine style={{ fontSize: "22px" }} />
               </div>
-            </a>
+            </motion.a>
           </div>
         </div>
         <Technologies />
