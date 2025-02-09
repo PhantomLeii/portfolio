adfe5764a24211de37eb62f3bb9ea50b01e8c09c
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-09 11:47:15 +0200

    update base font

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Geist, Geist_Mono } from 'next/font/google'
 import './styles.css'
 
 export const metadata = {
@@ -6,13 +7,18 @@ export const metadata = {
   title: 'Payload Blank Template',
 }
 
+const geistSans = Geist({
+  display: 'swap',
+  subsets: ['latin'],
+})
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
     <html lang="en">
       <body>
-        <main>{children}</main>
+        <main className={`${geistSans.className} antialiased`}>{children}</main>
       </body>
     </html>
   )
